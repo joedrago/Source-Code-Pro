66e0987518edf7eb46f6e205b354c3a18c8880e1
Joe Drago <joedrago@gmail.com>
2012-09-27 14:14:36 -0700

    * Renamed to "Dragon Mono", to maximize douchebaggy self-naming and allow for coexistence * Removed files I don't need to keep around (kept LICENSE to remind everyone of Adobe)

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set FAMILY=SourceCodePro
+set FAMILY=DragonMono
 
 del /S /F /Q target
 rd target
@@ -8,5 +8,5 @@ md target
 
 FOR %%W IN (Black,Bold,ExtraLight,Light,Regular,Semibold) DO (
 makeotf -sp target/%FAMILY%-%%W-otf.fpr -f Roman/%%W/font.pfa -r -o target/%FAMILY%-%%W.otf
-makeotf -sp target/%FAMILY%-%%W-ttf.fpr -f Roman/%%W/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/%FAMILY%-%%W.ttf
+REM makeotf -sp target/%FAMILY%-%%W-ttf.fpr -f Roman/%%W/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/%FAMILY%-%%W.ttf
 )
