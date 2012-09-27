0ad539f93086e8a887d4104282a82185ecf73935
Joe Drago <joedrago@gmail.com>
2012-09-27 11:24:32 -0700

    Add Windows friendly batch file

---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set FAMILY=SourceCodePro
+
+del /S /F /Q target
+rd target
+md target
+
+FOR %%W IN (Black,Bold,ExtraLight,Light,Regular,Semibold) DO (
+makeotf -sp target/%FAMILY%-%%W-otf.fpr -f Roman/%%W/font.pfa -r -o target/%FAMILY%-%%W.otf
+makeotf -sp target/%FAMILY%-%%W-ttf.fpr -f Roman/%%W/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/%FAMILY%-%%W.ttf
+)
